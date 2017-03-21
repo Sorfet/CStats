0cbd16b8065ab6e0b2a44a47fe2cde5261acf1c1
Sasu Mikonranta <sorfetti@gmail.com>
2017-03-21 19:20:17 +0200

    LIsätty Google Firebase tietokanta-alustus.

--- app/src/main/java/com/cstats/cstats/Main.java
+++ app/src/main/java/com/cstats/cstats/Main.java
@@ -3,8 +3,15 @@ package com.cstats.cstats;
 import android.icu.text.DecimalFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Sasu on 12.3.2017.
@@ -70,6 +77,45 @@ public class Main extends AppCompatActivity {
         catch(Exception e){
             e.printStackTrace();
         }
+
+        /*
+
+        ####Google firebase database operaatiot:
+
+//Kirjoita databaseen
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+
+//Lue databasesta
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                System.out.println("Value is: " + value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Failed to read value.\n"+ error.toException().toString());
+            }
+        });
+
+        */
+
+
+
 
     }
 }
